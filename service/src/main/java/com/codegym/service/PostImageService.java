09e293927211dc8b693bf49c8838c14432e63420
PhanMinhHuy <59156011+PhanMinhHuy@users.noreply.github.com>
2020-07-02 22:42:17 +0700

    add controller, service

--- service/src/main/java/com/codegym/service/PostImageService.java
+++ service/src/main/java/com/codegym/service/PostImageService.java
@@ -9,7 +9,7 @@ import java.util.Set;
 public interface PostImageService {
     Page<PostImage> findAll(Pageable pageable);
     PostImage findById(Long id);
-    Set<PostImage> findByPost(Long id);
+    Set<PostImage> findByPostId(Long id);
     void save(PostImage postImage);
     void deleteById(Long id);
 }
