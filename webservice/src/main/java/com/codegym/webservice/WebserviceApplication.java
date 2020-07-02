e77e3b0c15fc370e8b69ace8d770c447496b6de4
nht1206 <nghuutho74@gmail.com>
2020-07-02 23:51:02 +0700

    scan config

--- webservice/src/main/java/com/codegym/webservice/WebserviceApplication.java
+++ webservice/src/main/java/com/codegym/webservice/WebserviceApplication.java
@@ -7,11 +7,11 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.codegym.configuration")
 @EnableJpaRepositories(basePackages="com.codegym.dao.repository")
-@EnableJpaAuditing()
 @ComponentScan("com.codegym")
 @EntityScan("com.codegym.dao.model")
+@EnableJpaAuditing()
 public class WebserviceApplication {
 
     public static void main(String[] args) {
