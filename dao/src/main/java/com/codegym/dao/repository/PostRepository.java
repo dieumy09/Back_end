741623cfb7fbd05834d38d006cdbc2d42b69e7b5
Your Name <nguyenhanguyen912@gmail.com>
2020-07-03 16:27:23 +0700

    fix

--- dao/src/main/java/com/codegym/dao/repository/PostRepository.java
+++ dao/src/main/java/com/codegym/dao/repository/PostRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-    Page<Post> findPostsByUser_Id(Long userId, Pageable pageable);
+
 }
