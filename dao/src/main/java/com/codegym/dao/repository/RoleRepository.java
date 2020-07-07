fa39e7da843bc22d9881abd1c6c524f15a0e89d3
medivTheguardian <tranvanthuanaacc@gmail.com>
2020-07-07 10:56:46 +0700

    test

--- dao/src/main/java/com/codegym/dao/repository/RoleRepository.java
+++ dao/src/main/java/com/codegym/dao/repository/RoleRepository.java
@@ -4,4 +4,5 @@ import com.codegym.dao.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByRole(String role);
 }
