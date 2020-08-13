fc03f75d185156450abb694456491a1848894461
Your Name <nguyenhanguyen912@gmail.com>
2020-08-13 17:04:38 +0700

    filter by reason

--- dao/src/main/java/com/codegym/dao/repository/PostRepository.java
+++ dao/src/main/java/com/codegym/dao/repository/PostRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface PostRepository extends JpaRepository<Post, Long>, JpaSpecificationExecutor<Post> {
-//    Page<Post> findAllBy
     Page<Post> getPostsByUser_Id(Long userId, Pageable pageable);
+
     Page<Post> getPostsByUser_IdAndTitleContaining(Long userId, String title, Pageable pageable);
 }
